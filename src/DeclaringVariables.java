6aa2c2f2e9066b1287e6529359585c9d4670ac3a
tsadio <tsadio@montgomerycollege.edu>
2020-04-14 21:58:21 -0400

    first commit

---
+++ src/DeclaringVariables.java
@@ -0,0 +1,13 @@
+public class DeclaringVariables {
+    public static void main (String [] args) {
+
+        int testScore = 50;
+        double price = 24.99;
+        String userName = "Bobby Johnson";
+
+        System.out.println("Test Score initialized is: " + testScore);
+        System.out.println("Price tag: " + price);
+        System.out.println("Username: " + userName);
+
+    }
+}
